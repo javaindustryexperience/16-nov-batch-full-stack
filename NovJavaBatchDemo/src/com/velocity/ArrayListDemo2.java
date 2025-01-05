da74dde7c15d501e8373c6b77f199d6086f3c130
Jeevan <javaindustryexperience@gmail.com>
2025-01-05 08:21:37 +0530

    Arraylist code added for numbers

---
+++ NovJavaBatchDemo/src/com/velocity/ArrayListDemo2.java
@@ -0,0 +1,15 @@
+package com.velocity;
+
+import java.util.ArrayList;
+
+public class ArrayListDemo2 {
+
+	public static void main(String[] args) {
+
+		ArrayList<Integer> al = new ArrayList<Integer>();
+		al.add(10);
+		al.add(20);
+		al.add(30);
+		System.out.println(al);
+	}
+}

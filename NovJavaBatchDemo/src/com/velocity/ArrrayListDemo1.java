78fbbb5ea38e468c80d925582807dff3bc1ac354
Jeevan <javaindustryexperience@gmail.com>
2025-01-05 08:14:30 +0530

    Arraylist code added here for city

---
+++ NovJavaBatchDemo/src/com/velocity/ArrrayListDemo1.java
@@ -0,0 +1,16 @@
+package com.velocity;
+
+import java.util.ArrayList;
+
+public class ArrrayListDemo1 {
+
+	public static void main(String[] args) {
+
+		ArrayList<String> al = new ArrayList<String>();
+		al.add("pune");
+		al.add("mumbai");
+		al.add("bangalore");
+		System.out.println(al);
+	}
+
+}

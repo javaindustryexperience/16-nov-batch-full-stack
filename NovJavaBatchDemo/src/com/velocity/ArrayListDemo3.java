6e2c6b7dec8a245eae28d67e58d72d4093a884d3
Jeevan <javaindustryexperience@gmail.com>
2025-01-07 08:03:28 +0530

    Arraylist code added for integer and string

---
+++ NovJavaBatchDemo/src/com/velocity/ArrayListDemo3.java
@@ -0,0 +1,16 @@
+package com.velocity;
+
+import java.util.ArrayList;
+
+public class ArrayListDemo3 {
+
+	public static void main(String[] args) {
+
+		ArrayList al = new ArrayList();
+		al.add(50);
+		al.add(75);
+		al.add("vinit");
+		al.add("ram");
+		System.out.println(al);
+	}
+}
